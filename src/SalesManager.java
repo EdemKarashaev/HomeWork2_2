b62f3a7c05c65ac330ede0429badc4f59e0a4993
Edem <just_2_feel@mail.ru>
2022-10-19 16:01:00 +0300

    first

---
+++ src/SalesManager.java
@@ -0,0 +1,19 @@
+public class SalesManager {
+    protected int [] sales;
+    int max = -1;
+
+    public SalesManager(int[] sales) {
+        this.sales = sales;
+    }
+
+    //вычисление максимального значения массива
+    public int max() {
+        for (int sale : sales) {
+            if (sale > max) {
+                max = sale;
+            }
+        }
+        return max;
+    }
+
+}
